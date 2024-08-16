4d53076619ffb040e0f9434a55382df76617f7ab
alenash1988 <Aleonash1988@mail.ru>
2024-08-16 21:43:13 +0900

    Add files via upload

---
+++ modul_1_6.py
@@ -0,0 +1,13 @@
+my_dict = {'Kira': 2012, 'krstina': 2014, 'Gurii': 2020}
+print(my_dict)
+print(my_dict['Kira'])
+print(my_dict.get('Alena'))
+my_dict.update({'Tany': 2000, 'Sony': 2003})
+print(my_dict.pop('Kira'))
+print(my_dict)
+my_set = {1, 2, 5, 6, 1, 2, 5, "pops", True, (1,2,3)}
+print(my_set)
+my_set.add(7)
+my_set.add(8)
+my_set.discard(2)
+print(my_set)
